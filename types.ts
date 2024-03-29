f2ee0b8df557a8a1af29aa1e6331e132d5085b5a
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-03-28 21:13:53 -0500

    feat(conversation setup): add conversations setup

--- types.ts
+++ types.ts
@@ -1,5 +1,10 @@
-import type { Member, Profile, Server } from '@prisma/client';
+import type { Conversation, Member, Profile, Server } from '@prisma/client';
 
 export type ServerWithMembersWithProfiles = Server & {
 	members: Array<Member & { profile: Profile }>;
+};
+
+export type ConversationWithMembersWithProfiles = Conversation & {
+	memberOne: Member & { profile: Profile };
+	memberTwo: Member & { profile: Profile };
 };
